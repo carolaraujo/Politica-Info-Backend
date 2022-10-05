e64e67622f039f3783d2c00f46ebb0d669c52b74
antoniopavao <antonioppavao@gmail.com>
2022-10-04 22:22:33 -0300

    Add swagger to generate documentation for the app.

--- src/main/java/com/faculdadeImpacta/politicainfobackend/PoliticaInfoBackendApplication.java
+++ src/main/java/com/faculdadeImpacta/politicainfobackend/PoliticaInfoBackendApplication.java
@@ -4,10 +4,11 @@ import org.modelmapper.ModelMapper;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
-
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
 
 @SpringBootApplication
+@EnableSwagger2
 public class PoliticaInfoBackendApplication {
 
 	@Bean
